4f8b41eabcf8061c80af751d335812a048039f89
Slash Rahul <rahul.mahale123@gmil.com>
2015-12-28 09:01:53 -0500

    deploying rails app

--- cookbooks/deploy/recipes/default.rb
+++ cookbooks/deploy/recipes/default.rb
@@ -20,7 +20,18 @@ cwd '/home/rahul/wheel/'
 user 'rahul'
 code <<-EOH
 cd /home/rahul/wheel/
-source ~/.rvm/scripts/rvm && 	bundle install
-cp config/database.yml.postgresqlapp config/database.yml
+	source ~/.rvm/scripts/rvm && 	bundle install
+	cp config/database.yml.postgresqlapp config/database.yml
+	rake db:migrate
+        rake assets:precompile
+        sudo service unicorn_wheel stop
+        sleep 15
+        sudo service unicorn_wheel start
+        sudo service delayed_job stop
+        sleep 15
+        sudo service delayed_job start
+
+        sudo service nginx restart
+
 EOH
 end
